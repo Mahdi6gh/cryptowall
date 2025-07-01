caddb5e7f639e7356fad8d5c79b7d4db29f963b0
Mahdi6gh <pesarekhobb85@gmail.com>
2025-07-01 06:54:48 +0330

    RSA key
    creating RSA key generator

---
+++ generateKeys.py
@@ -0,0 +1,9 @@
+from Crypto.PublicKey import RSA
+key=RSA.generate(2048)
+privet_key=key.export_key()
+with open("private.pem", "wb") as f:
+    f.write(privet_key)   
+public_key=key.publickey().export_key()
+with open("publick.pem", "wb") as f:
+    f.write(public_key)
+print("✅ RSA keys created and saved")
